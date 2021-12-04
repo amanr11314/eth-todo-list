7c80cd357e08304fac9e74ff57b8c5e7875c3c6a
amanr11314 <amanr11314@gmail.com>
2021-12-05 03:28:20 +0530

    DONE: created the DAPP

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,45 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
-    uint256 public taskCount = 0;
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
+    constructor () public {
+        createTask("Check out blockhain develoment");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount,_content,false);
+
+        emit TaskCreated(taskCount,_content,false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+
+        emit TaskCompleted(_id,_task.completed);
+    }
+
+
 }
